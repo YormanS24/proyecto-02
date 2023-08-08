3f21e7c7edfa6487dc09f9d13c5e4c6c08517d1e
YormanS24 <sanchezyorman0@gmail.com>
2023-08-08 16:26:43 -0500

    proyecto-02 V1.1.3

--- Modules/Category/Providers/CategoryServiceProvider.php
+++ Modules/Category/Providers/CategoryServiceProvider.php
@@ -4,6 +4,8 @@ namespace Modules\Category\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Factory;
+use Modules\Category\Modules\Category\Service\CategoryService;
+use Modules\Category\Service\CategoryInterface;
 
 class CategoryServiceProvider extends ServiceProvider
 {
@@ -38,6 +40,7 @@ class CategoryServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->register(RouteServiceProvider::class);
+        $this->app->bind(CategoryInterface::class, CategoryService::class);
     }
 
     /**
